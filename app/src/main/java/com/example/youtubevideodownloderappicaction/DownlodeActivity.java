1c4e408fcd9a2e92246d5c3d23208162ddbff706
Munesh Sutar <85051705+muneshsutar@users.noreply.github.com>
2021-12-04 23:41:24 +0530

    Fast LayoutDesign

---
+++ app/src/main/java/com/example/youtubevideodownloderappicaction/DownlodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.youtubevideodownloderappicaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DownlodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downlode);
+    }
+}
