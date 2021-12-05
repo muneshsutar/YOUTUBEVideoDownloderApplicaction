299053858545b5f1609b7559172381083c574b9c
Munesh Sutar <85051705+muneshsutar@users.noreply.github.com>
2021-12-05 14:24:50 +0530

    Second Layout and Some backend Update

--- app/src/main/java/com/example/youtubevideodownloderappicaction/DownlodeActivity.java
+++ app/src/main/java/com/example/youtubevideodownloderappicaction/DownlodeActivity.java
@@ -3,12 +3,48 @@ package com.example.youtubevideodownloderappicaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DownlodeActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText ed1;
+
+    private WebView wv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_downlode);
+        b1 = findViewById(R.id.findvideoBtn);
+        ed1 = findViewById(R.id.getvideolink);
+
+        wv1 = findViewById(R.id.WebView);
+        wv1.setWebViewClient(new MyBrowser());
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ed1.getText().toString();
+
+                wv1.getSettings().setLoadsImagesAutomatically(true);
+                wv1.getSettings().setJavaScriptEnabled(true);
+
+                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                wv1.loadUrl(url);
+            }
+        });
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
